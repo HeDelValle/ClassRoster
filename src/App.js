7d5dfc1ad2f2d2f00cc888b2c55a7b9cb72bc99a
HeDelValle <72476230+HeDelValle@users.noreply.github.com>
2021-12-08 17:58:48 -0800

    Added more arrays

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
 import "./styles.css";
 
 export default function App() {
+  let elements = [];
+
+  let people = [
+    ["Victor", "Lymar"],
+    ["Traci", "Siebel"]
+  ];
+  for (let person of people) {
+    console.log(person);
+
+    let firstName = person[0];
+    let lastName = person[1];
+    elements.push(displayPerson(firstName, lastName));
+  }
   return (
     <div className="App">
       <h1>Class List</h1>
       <h2>Table</h2>
-
-        
-      
+      <div> {elements}</div>
     </div>
   );
 }
-//function to store the date 
-function ClassRoster (first,last, year, color) {
- return first + " " + last +  " " + year + " " + color
+//function to store the date
+function ClassRoster(first, last, year, color) {
+  return first + " " + last + " " + year + " " + color;
 }
 
-//variables to take the people's info 
+function displayPerson(first, last) {
+  return (
+    <div>
+      {" "}
+      {first} {last}
+    </div>
+  );
+}
 
-let firstPerson = ClassRoster("Viktor", "Lymar", "1923", "green");
-let secondPerson = ClassRoster("Traci", "Siebel", "2022", "purple");
-let thirdPerson= ClassRoster("Ida", "Voong", "2022", "blue");
-let fourthPerson= ClassRoster("Ana", "Rivera", "2051", "green");
-let fifthPerson= ClassRoster("Aidan", "Fuller", "2023", "red") 
-let sixthPerson= ClassRoster("Kyle", "Wan", "2023", "Yeah")
-let seventhPerson= ClassRoster("Enjun", "Li", "2022", "black")
-let eigthPerson= ClassRoster ("Luis", "Garcia", "2025", "black")
-let ninthPerson= ClassRoster ("Amanda", "Yu", "2022", "blue")
-let tenthPerson= ClassRoster ("Yifei", "Du", "2022", "Black")
-let lastPerson= ClassRoster ("Hector", "Del Valle", "2022", "Yellow")
+//variables to take the people's info
 
-console.log(firstPerson);
-console.log(secondPerson);
-console.log(thirdPerson);
-console.log(fourthPerson);
-console.log(fifthPerson);
-console.log(sixthPerson);
-console.log(seventhPerson);
-console.log(eigthPerson);
-console.log(ninthPerson);
-console.log(tenthPerson);
-console.log(lastPerson);
+let firstPerson = ["Viktor", "Lymar", "1923", "green"];
+let secondPerson = ["Traci", "Siebel", "2022", "purple"];
+let thirdPerson = ["Ida", "Voong", "2022", "blue"];
+let fourthPerson = ["Ana", "Rivera", "2051", "green"];
+let fifthPerson = ["Aidan", "Fuller", "2023", "red"];
+let sixthPerson = ["Kyle", "Wan", "2023", "Yeah"];
+let seventhPerson = ["Enjun", "Li", "2022", "black"];
+let eigthPerson = ["Luis", "Garcia", "2025", "black"];
+let ninthPerson = ["Amanda", "Yu", "2022", "blue"];
+let tenthPerson = ["Yifei", "Du", "2022", "Black"];
+let lastPerson = ["Hector", "Del Valle", "2022", "Yellow"];
+
+//console.log(firstPerson);
+//console.log(secondPerson);
+//console.log(thirdPerson);
+//console.log(fourthPerson);
+//console.log(fifthPerson);
+//console.log(sixthPerson);
+//console.log(seventhPerson);
+//console.log(eigthPerson);
+//console.log(ninthPerson);
+//console.log(tenthPerson);
+//console.log(lastPerson);
